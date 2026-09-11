f1a6d9e9217ecb3d1058b62e67656ce09975ab0c
Ihor Kublytskyi <ihorkublytskyi@gmail.com>
2026-09-11 16:26:24 +0200

    feat: add initial migration

---
+++ backend/dZENcode.Persistence/Migrations/20260911132008_Init.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dZENcode.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Core");
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                schema: "Core",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ParentCommentId = table.Column<int>(type: "int", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    HomePageUrl = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Body = table.Column<string>(type: "nvarchar(1024)", maxLength: 1024, nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    AttachmentPath = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_Comments_ParentCommentId",
+                        column: x => x.ParentCommentId,
+                        principalSchema: "Core",
+                        principalTable: "Comments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_Email",
+                schema: "Core",
+                table: "Comments",
+                column: "Email");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_ParentCommentId_CreatedAt",
+                schema: "Core",
+                table: "Comments",
+                columns: new[] { "ParentCommentId", "CreatedAt" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_Username",
+                schema: "Core",
+                table: "Comments",
+                column: "Username");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments",
+                schema: "Core");
+        }
+    }
+}
