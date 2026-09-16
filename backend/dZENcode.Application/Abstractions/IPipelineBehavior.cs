e6ca0afdd175ab2b10128620b93836d31c23130e
Ihor Kublytskyi <ihorkublytskyi@gmail.com>
2026-09-16 20:09:32 +0200

    feat: add pipeline behavior support with validation

---
+++ backend/dZENcode.Application/Abstractions/IPipelineBehavior.cs
@@ -0,0 +1,12 @@
+﻿using dZENcode.Application.Features.Dispatcher;
+
+namespace dZENcode.Application.Abstractions;
+
+public interface IPipelineBehavior<in TInstruction, TResponse> 
+	where TInstruction : IInstruction<TResponse>
+{
+	Task<TResponse> HandleAsync(
+		TInstruction instruction,
+		InstructionHandlerDelegate<TResponse> next,
+		CancellationToken cancellationToken = default);
+}
